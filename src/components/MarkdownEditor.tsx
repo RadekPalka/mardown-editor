e1f24537c1b819c027f7e2286f4419da1fdaecda
Radosław Pałka <radekpalka@gmail.com>
2025-02-15 20:42:27 +0100

    add toogleBold function

--- src/components/MarkdownEditor.tsx
+++ src/components/MarkdownEditor.tsx
@@ -1,11 +1,20 @@
 import DOMPurify from 'dompurify';
 import { signal } from '@preact/signals-react';
+import { useEffect, useRef } from 'react';
 
 export const markdownText = signal('');
+export const textareaRef = signal<HTMLTextAreaElement | null>(null);
 
 export const MarkdownEditor: React.FC = () => {
+	const ref = useRef<HTMLTextAreaElement>(null);
+
+	useEffect(() => {
+		textareaRef.value = ref.current;
+	}, []);
+
 	return (
 		<textarea
+			ref={ref}
 			className='editor-preview-style'
 			onChange={(e) => (markdownText.value = DOMPurify.sanitize(e.target.value))}
 		></textarea>
--- src/components/Toolbar.tsx
+++ src/components/Toolbar.tsx
@@ -1,10 +1,45 @@
 import { faBold, faItalic } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { textareaRef, markdownText } from './MarkdownEditor';
+import { useSignals } from '@preact/signals-react/runtime';
 
 export const Toolbar: React.FC = () => {
+	const boldItalicRegex = /^(\*\*\*|___)(.*?)\1$/;
+	const boldRegex = /^(\*\*|__)(.*?)\1$/;
+	const italicRegex = /^(\*|_)(.*?)\1$/;
+
+	useSignals();
+	const toggleBold = () => {
+		const textArea = textareaRef.value;
+
+		if (!textArea) return;
+		let newText: string;
+		const startIndex = textArea.selectionStart;
+		const endIndex = textArea.selectionEnd;
+		const selectedText = textArea.value.substring(startIndex, endIndex);
+
+		if (boldRegex.test(selectedText)) {
+			newText = selectedText.slice(2, selectedText.length - 2);
+			console.log(newText);
+		} else {
+			newText = `**${selectedText}**`;
+		}
+		const updatedMarkdownText =
+			markdownText.value.slice(0, startIndex) +
+			newText +
+			markdownText.value.slice(endIndex);
+		markdownText.value = updatedMarkdownText;
+		textArea.value = updatedMarkdownText;
+		console.log(updatedMarkdownText);
+	};
 	return (
 		<div className='flex gap-6 bg-black py-4 px-4 w-full h-auto'>
-			<FontAwesomeIcon icon={faBold} color='white' className='cursor-pointer' />
+			<FontAwesomeIcon
+				icon={faBold}
+				color='white'
+				className='cursor-pointer'
+				onClick={toggleBold}
+			/>
 			<FontAwesomeIcon icon={faItalic} color='white' className='cursor-pointer' />
 		</div>
 	);
